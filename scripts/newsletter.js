3d34230b995cf73955cb2e08d2b9325bf32c6fc2
Guilherme Henrique Portigliotti <guilherme.henrique@compasso.com.br>
2019-09-09 09:55:28 -0300

    alteração de layout e na verificação de e-mail do newsletter

--- scripts/newsletter.js
+++ scripts/newsletter.js
@@ -1,13 +1,14 @@
 function submitNewsletter() {
-  event.preventDefault();
+    event.preventDefault();
+    var msgFeedback = document.getElementsByClassName("msgFeedback");
     var email = $("#email").val();
     var separaMetade = email.substr(email.indexOf("@"));
-    var verificaPonto = separaMetade.indexOf(".");
-    console.log("teste: " + verificaPonto);
-		if(email != "" && email.indexOf("@") != -1 && verificaPonto != -1){
-    alert("Seu email foi cadastrado com sucesso!");
+    // var verificaPonto = separaMetade.indexOf(".");
+    console.log("DAWDAW: " + separaMetade.length);
+		if(email != "" && email.indexOf("@") != -1 && separaMetade.length > 1){
+      msgFeedback[0].setAttribute("data-success", "E-mail cadastrado com sucesso!");
     $("#email").val("");
   }else {
-    alert("Ocorreu um erro, por favor verifique seu email e tente novamente!");
+    msgFeedback[0].setAttribute("data-error", "Por favor, digite um e-mail valido!");
   }
 }
