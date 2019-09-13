c6ba061b4a61996947b0311a667888df4bb8a224
Guilherme Henrique Portigliotti <guilherme.henrique@compasso.com.br>
2019-09-13 17:22:48 -0300

    ajustes no banner da home

---
+++ scripts/ajuste-tam-home-banner.js
@@ -0,0 +1,9 @@
+$('.img-bannerHome').replaceWith(function(i, v){
+  console.log("ALOOOOOOOOOOUUUUU");
+    return $('<div/>', {
+        style: 'background-image: url(' + this.src + ');' +
+        'width:' + this.width + 'px;' +
+        'height:' + this.height + 'px;' ,
+        class: 'fakeImg'
+    })
+})
