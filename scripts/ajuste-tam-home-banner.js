36f862fdfc1f3ed333816859f3613084c7a06c43
Guilherme Henrique Portigliotti <guilherme.henrique@compasso.com.br>
2019-09-16 15:32:54 -0300

    ajuste no tamanho dos icones dos produtos e ajuste no icone do yt no footer, adicionado botoes de comprar aos cards de produtos tambem

--- scripts/ajuste-tam-home-banner.js
+++ scripts/ajuste-tam-home-banner.js
@@ -1,4 +1,4 @@
-$('.img-bannerHome').replaceWith(function(i, v){
+$('.muito-top').replaceWith(function(i, v){
   console.log("ALOOOOOOOOOOUUUUU");
     return $('<div/>', {
         style: 'background-image: url(' + this.src + ');' +
--- scripts/newsletter.js
+++ scripts/newsletter.js
@@ -3,12 +3,15 @@ function submitNewsletter() {
     var msgFeedback = document.getElementsByClassName("msgFeedback");
     var email = $("#email").val();
     var separaMetade = email.substr(email.indexOf("@"));
-    // var verificaPonto = separaMetade.indexOf(".");
-    console.log("DAWDAW: " + separaMetade.length);
-		if(email != "" && email.indexOf("@") != -1 && separaMetade.length > 1){
+    var verificaPonto = email.indexOf(".");
+		if(email != "" && email.indexOf("@") != -1 && separaMetade.length > 1 && verificaPonto != -1){
+      $("#email").removeClass("invalid");
+      $("#email").addClass("valid");
+      $("#email").val("");
       msgFeedback[0].setAttribute("data-success", "E-mail cadastrado com sucesso!");
-    $("#email").val("");
   }else {
+    $("#email").removeClass("valid");
+    $("#email").addClass("invalid");
     msgFeedback[0].setAttribute("data-error", "Por favor, digite um e-mail valido!");
   }
 }
